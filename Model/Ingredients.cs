a08371f8fa2531faca24a4f5d12902a35c996352
Simon Palm <sp222ym@student.lnu.se>
2021-06-11 09:16:05 +0200

    Moving code to the view and adding getters and setters.

--- Model/Ingredients.cs
+++ Model/Ingredients.cs
@@ -2,98 +2,15 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 
-namespace View
+namespace Model
 {
     class Ingredients
     {
-        private string _userMessage;
+        public string ingredientName {get; set;}
 
-        public string UserMessage
-        {
-            get { return _userMessage; }
-            set
-            {
-                _userMessage = value;
-            }
-        }
-        private  List<String> Ingredient = new List<String>();
-        public Ingredients() {
-            Console.WriteLine("Welcome to recipes");
-        }
+        public string ingredientUnit {get; set;}
 
-        public void TempRecipes() {
-        Console.WriteLine("Gingerbread biscuit; 100, pieces; [1, litre, flour; 2, dl, cream; 1, spoon, cinnamon]; *Mix everything, *Wait one day, *Make figures");
-        Console.WriteLine("Pancakes; 20, pieces; [1, litre, flour; 2, dl, cream; 1, spoon, cinnamon]; *Mix everything, *Wait one day, *Make figures");
-        Console.WriteLine("Meatballs; 40, pieces; [1, litre, flour; 2, dl, cream; 1, spoon, cinnamon]; *Mix everything, *Wait one day, *Make figures");
+        public int ingredientPrice {get; set;}
 
-        }
-
-        public void IngredientsMenu() {
-        Console.Clear();
-        Console.WriteLine("Press 1 To add ingredients");
-        Console.WriteLine("Press 2 To go back");
-          if (UserMessage != null)
-            {
-                Console.WriteLine(UserMessage);
-                Console.WriteLine("");
-                UserMessage  = null;
-            }
-          try
-            {
-                int userChoice = Int16.Parse(getUserChoice());
-                if (userChoice == 1)
-                {
-                    AddIngredients();
-                }
-                else if (userChoice == 2)
-                {
-                    Console.WriteLine("Implement back to menu");
-                }
-                else
-                {
-                    UserMessage = "Please enter the number 1 or 2.";
-                     IngredientsMenu();
-                }
-            }
-            catch (Exception)
-            {
-                UserMessage = "Please enter a valid number instead!";
-                IngredientsMenu();
-            }
-        }
-        
-        private void AddIngredients() {
-        Console.Clear();
-        Console.WriteLine("Add your ingredient");
-        Console.WriteLine("Enter name of the ingredient");
-        string ingredientName = getUserChoice();
-        Console.Clear();
-        Console.WriteLine("Enter unit");
-        string ingredientUnit = getUserChoice();
-        Console.Clear();
-        Console.WriteLine("Enter the price of the ingredient");
-        string ingredientPrice = getUserChoice();
-
-        Console.Clear();
-        
-
-        Console.WriteLine("Press 1 To add another ingredient");
-        Console.WriteLine("Press 2 To go back");
-        int userChoice = Int16.Parse(getUserChoice());
-                if (userChoice == 1)
-                {
-                    AddIngredients();
-                }
-                else if (userChoice == 2)
-                {
-                    Console.WriteLine("Implement back to menu 2");
-                }
-     
-        }
-
-         public string getUserChoice()
-        {
-            return Console.ReadLine();
-        }
     }
 }
