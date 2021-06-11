26c73dcd1e834b9c83393d91858cfd0a8c1fe3db
Simon Palm <sp222ym@student.lnu.se>
2021-06-11 11:42:34 +0200

    Adding temporary content.

--- View/ViewRecipes.cs
+++ View/ViewRecipes.cs
@@ -1,3 +1,20 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+
+namespace View
+{
+    class ViewRecipes
+    {
+        public ViewRecipes() {
+            TempRecipes();
+        }
+
+        public void TempRecipes() {
+        Console.WriteLine("Gingerbread biscuit; 100, pieces; [1, litre, flour; 2, dl, cream; 1, spoon, cinnamon]; *Mix everything, *Wait one day, *Make figures");
+        Console.WriteLine("Pancakes; 20, pieces; [1, litre, flour; 2, dl, cream; 1, spoon, cinnamon]; *Mix everything, *Wait one day, *Make figures");
+        Console.WriteLine("Meatballs; 40, pieces; [1, litre, flour; 2, dl, cream; 1, spoon, cinnamon]; *Mix everything, *Wait one day, *Make figures");
+        }
+        
+    }
+}
