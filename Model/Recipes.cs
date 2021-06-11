22e2006c1074ad3e1e07f44aa21d539863eecf73
Simon Palm <sp222ym@student.lnu.se>
2021-06-11 09:16:39 +0200

    Fixing recipes with getters and setters.

--- Model/Recipes.cs
+++ Model/Recipes.cs
@@ -1,3 +1,20 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+
+namespace Model
+{
+    class Recipes
+    {
+        public string recipeName {get; set;}
+
+        public int recipePortions {get; set;}
+
+        public string recipeIngredient {get; set;}
+
+        public int recipeIngredientAmount {get; set;}
+
+        public string recipeComment {get; set;}
+    }
+}
+
