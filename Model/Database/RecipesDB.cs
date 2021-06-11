e9858c89f19d1a2d9d3e92484210c5a55aa20724
Simon Palm <sp222ym@student.lnu.se>
2021-06-11 11:40:40 +0200

    Adding database.

---
+++ Model/Database/RecipesDB.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Database
+{
+    class RecipesDB
+    {
+        Model.Recipes m_recipes = new Model.Recipes();
+          private string _dbFileName = "RecipeDB.txt";
+
+         public void saveRecipe() {
+       
+            if (!File.Exists(_dbFileName))
+            {
+                StreamWriter sw = File.CreateText(_dbFileName);
+            {
+                        sw.WriteLine($"Recipe:{m_recipes.RecipeName}, Portions:{m_recipes.RecipePortions}, Ingredients:{m_recipes.RecipeIngredient}, IngredientAmount:{m_recipes.RecipeIngredientAmount}, Comments:{m_recipes.RecipeComment}");   
+            }
+                        sw.Close();
+            }
+            else
+            {
+                StreamWriter sw = File.AppendText(_dbFileName);
+                {
+     
+                   sw.WriteLine($"Recipe:{m_recipes.RecipeName}, Portions:{m_recipes.RecipePortions}, Ingredients:{m_recipes.RecipeIngredient}, IngredientAmount:{m_recipes.RecipeIngredientAmount}, Comments:{m_recipes.RecipeComment}");   
+
+                }
+                 sw.Close();   
+            }
+        }
+
+    }
+}
