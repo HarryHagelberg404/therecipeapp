defe31aa772dce6028e24272ea08b4e330a2eafa
Simon Palm <sp222ym@student.lnu.se>
2021-06-11 11:39:22 +0200

    Fixing getters and setters.

--- Model/Ingredients.cs
+++ Model/Ingredients.cs
@@ -6,11 +6,25 @@ namespace Model
 {
     class Ingredients
     {
-        public string ingredientName {get; set;}
+        private string _ingredientName;
 
-        public string ingredientUnit {get; set;}
+        private string _ingredientUnit;
 
-        public int ingredientPrice {get; set;}
+        private int _ingredientPrice;
+        public string IngredientName {
+        get {return _ingredientName;} 
+        set {_ingredientName = value;}
+        }
+
+        public string IngredientUnit {
+        get {return _ingredientUnit;} 
+        set {_ingredientUnit = value;}
+        }
+
+        public int IngredientPrice {
+        get {return _ingredientPrice;} 
+        set {_ingredientPrice = value;}
+        }
 
     }
 }
