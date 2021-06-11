d7997fd4a4edadb5e4bfb59a3bb00b8ca86b7c68
Simon Palm <sp222ym@student.lnu.se>
2021-06-11 15:11:39 +0200

    Moving menu to a view file.

--- Controller/CookBook.cs
+++ Controller/CookBook.cs
@@ -6,68 +6,15 @@ namespace Controller
 {
     class CookBook
     {
-        View.Recipes v_recipes = new View.Recipes();
-
-        View.ViewRecipes v_viewRecipes = new View.ViewRecipes();
-        View.Ingredients v_ingredients = new View.Ingredients();
-
-        Database.IngredientDB m_ingredientDB = new Database.IngredientDB();
-
-        Database.RecipesDB m_recipeDB = new Database.RecipesDB();
-        
+        View.CookBookStartMenu v_CookBookStartMenu = new View.CookBookStartMenu();
+      
       
         public CookBook() 
         {
-            CookBookMenu();
+           v_CookBookStartMenu.CookBookMenu();
         }
 
-        public void CookBookMenu() {
-          Console.Clear();
-          Console.WriteLine("Welcome to your digital Cookbook");
-          Console.WriteLine("Press 1 to view your recipes");
-          Console.WriteLine("Press 2 to add ingredients");
-          Console.WriteLine("Press 3 to add recipe");
-          Console.WriteLine("Press 4 to exit the application");
-           if (v_recipes.UserMessage != null)
-            {
-                Console.WriteLine(v_recipes.UserMessage );
-                Console.WriteLine("");
-                v_recipes.UserMessage = null;
-            }
-
-             try
-            {
-                int userChoice = Int16.Parse(v_recipes.getUserChoice());
-                if (userChoice == 1)
-                {
-                    v_viewRecipes.TempRecipes();
-                }
-                else if (userChoice == 2)
-                {
-                    v_ingredients.IngredientsMenu();
-                } 
-                else if (userChoice == 3)
-                {
-                    v_recipes.RecipesMenu();
-                } 
-                 else if (userChoice == 4)
-                {
-                  m_ingredientDB.saveIngredient();
-                  m_recipeDB.saveRecipe();
-                  System.Environment.Exit(0);
-                }
-                else
-                {
-                    v_recipes.UserMessage  = "Please enter the number 1,2,3 or 4.";
-                    CookBookMenu();
-                }
-            }
-            catch (Exception)
-            {
-                v_recipes.UserMessage  = "Please enter a valid number instead!";
-                CookBookMenu();
-            }
-        }
+      
     }
 
 }
