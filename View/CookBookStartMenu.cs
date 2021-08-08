e54c5535e9a183b07ded3c095dd03d2790567ca6
Simon Palm <sp222ym@student.lnu.se>
2021-08-08 14:15:22 +0200

    Creating a new controller which all files have a reference to.

--- View/CookBookStartMenu.cs
+++ View/CookBookStartMenu.cs
@@ -12,6 +12,10 @@ namespace View
         Database.IngredientDB m_ingredientDB = new Database.IngredientDB();
         Database.RecipesDB m_recipeDB = new Database.RecipesDB();
 
+        Controller.Ingredients c_ingredients = new Controller.Ingredients();
+
+        Controller.Recipes c_recipes = new Controller.Recipes();
+
         private string _userMessage;
 
         public string UserMessage
@@ -49,17 +53,17 @@ namespace View
                 }
                 else if (userChoice == 2)
                 {
-                    v_ingredients.IngredientsMenu();
+                    v_ingredients.IngredientsMenu(c_ingredients);
                 } 
                 else if (userChoice == 3)
                 {
-                    v_recipes.RecipesMenu();
+                    v_recipes.RecipesMenu(c_recipes);
                 } 
                  else if (userChoice == 4)
                 {
-                  m_ingredientDB.saveIngredient();
-                  m_recipeDB.saveRecipe();
-                  System.Environment.Exit(0);
+                  m_ingredientDB.saveIngredient(c_ingredients);
+                  m_recipeDB.saveRecipe(c_recipes);
+                  System.Environment.Exit(1);
                 }
                 else
                 {
