c9c7312a6cd3dc28b7946cd84aaa753e813cf7a2
Harry <hh222wf@student.lnu.se>
2021-08-16 15:13:44 +0200

    Movingf Recipe to model bcs of MVC

---
+++ Model/Recipe.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Model
+{
+    class Recipe
+    {  
+        private string _recipeName;
+
+        private int _recipePortions;
+
+        private string _recipeIngredients;
+
+        private int _recipeIngredientsAmount;
+
+        private string _recipeIngredientsComment;
+
+        private List<string> _recipeInstructions;
+
+        private string _recipeComment;
+        
+        public string RecipeName {
+        get {return _recipeName;} 
+        set {_recipeName = value;}
+        }
+
+        public int RecipePortions {
+        get {return _recipePortions;} 
+        set {_recipePortions = value;}
+        }
+
+        public string RecipeIngredient {
+        get {return _recipeIngredients;} 
+        set {_recipeIngredients = value;}
+        }
+
+        public int RecipeIngredientAmount {
+        get {return _recipeIngredientsAmount;} 
+        set {_recipeIngredientsAmount = value;}}
+
+        public string RecipeComment {
+        get {return _recipeComment;} 
+        set {_recipeComment = value;}
+        }
+    }
+}
